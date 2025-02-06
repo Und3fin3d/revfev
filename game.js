a3bfe78034c227768472e25177a81cac9b3ac030
Und3fin3d <the.william.greenfield@gmail.com>
2025-02-06 03:45:36 +0000

    better drag

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 revFev = document.getElementById('revFev')
 quackSound = new Audio('quack.mp3')
 let beingDragged = false
-let e = 0.6
+let e = 0.7
 let g = 0.6
-let fr = 0.05
-let ρ = 0.2
+let fr = 0.04
+let ρ = 0.0002
 let x = (window.innerWidth - revFev.offsetWidth) / 2
 let y = (window.innerHeight - revFev.offsetHeight) / 2
 let ddx = 0
@@ -16,41 +16,30 @@ revFev.style.left = x + 'px'
 revFev.style.top = y + 'px'
 
 function moveFev() {
-    if (!beingDragged){
-        dy += g
+    console.log(Math.round(dx), Math.round(dy));
+    if (!beingDragged) {
+      dy += g;
+      dy += (dy !== 0 ? (dy > 0 ? -ρ * dy ** 2 : ρ * dy ** 2) : 0);
+      dx += (dx !== 0 ? (dx > 0 ? -ρ * dx ** 2 : ρ * dx ** 2) : 0);
     }
-    x += dx
-    y += dy
-    if (dy > 0) {
-        dy = dy - ρ
-    } else if (dy < 0) {
-        dy = dy + ρ
-    }
-    if (dx > 0) {
-        dx = dx - ρ
-    } else if (dx < 0) {
-        dx = dx + ρ
-    }
-    if (x + revFev.offsetWidth > window.innerWidth) {
-        x = window.innerWidth - revFev.offsetWidth
-        dx = -dx * e
-    } else if (x < 0) {
-        x = 0
-        dx = -dx * e
+    x += dx;
+    y += dy;
+    if (x < 0) {
+      x = 0;
+      dx = -dx * e;
+    } else if (x + revFev.offsetWidth > window.innerWidth) {
+      x = window.innerWidth - revFev.offsetWidth;
+      dx = -dx * e;
     }
     if (y + revFev.offsetHeight > window.innerHeight) {
-        y = window.innerHeight - revFev.offsetHeight
-        dy = -dy * e
-        if (dx > 0) {
-            dx = Math.max(0, dx - fr);
-        } else if (dx < 0) {
-            dx = Math.min(0, dx + fr);
-        }
+      y = window.innerHeight - revFev.offsetHeight;
+      dy = -dy * e;
+      dx = dx > 0 ? Math.max(0, dx - fr) : dx < 0 ? Math.min(0, dx + fr) : 0;
     }
-    revFev.style.left = x + 'px'
-    revFev.style.top = y + 'px'
-    requestAnimationFrame(moveFev)
-}
+    revFev.style.left = x + 'px';
+    revFev.style.top = y + 'px';
+    requestAnimationFrame(moveFev);
+  }
 
 revFev.addEventListener('dragstart', function(e) {
     e.preventDefault()
